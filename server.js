0864631538847b924a5ff4d9d426bf37901a1a23
cody-mick <cmick@byui.edu>
2022-11-21 12:14:33 -0700

    👌 IMPROVE: Models set up and sequence generator imported

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var http = require("http");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var mongoose = require("mongoose");
 
 // import the routing file to handle the default (index) route
 var index = require("./server/routes/app");
@@ -14,6 +15,16 @@ const messageRoutes = require("./server/routes/messages");
 const contactRoutes = require("./server/routes/contacts");
 const documentRoutes = require("./server/routes/documents");
 const { use } = require("./server/routes/app");
+
+// establish connection to the mongo database
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.dpz94eu.mongodb.net/?retryWrites=true&w=majority`,
+  { useNewUrlParser: true },
+  (err, res) => {
+    if (err) console.log("Connection failed: ", err);
+    console.log("Successfully connected to database!");
+  }
+);
 
 var app = express(); // create an instance of express
 
---
+++ server/models/document.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const documentSchema = mongoose.Schema({
+  id: { type: String, required: true },
+  name: { type: String },
+  url: { type: String },
+  children: [
+    { id: { type: String }, name: { type: String }, url: { type: String } },
+  ],
+});
+
+module.exports = mongoose.model("Document", documentSchema);
